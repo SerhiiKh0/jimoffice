da48896c5d9d721c690c9779b67537724c20d1a1
SerhiiKh0 <kharytoniuk_ak23@nuwm.edu.ua>
2024-11-14 23:34:51 +0200

    add static

--- mysite/settings.py
+++ mysite/settings.py
@@ -131,7 +131,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 import os
 STATIC_URL = '/static/'
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'app_blog/../static')
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
